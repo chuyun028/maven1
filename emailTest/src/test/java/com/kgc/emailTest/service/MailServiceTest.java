b2af95d8da2c639d74568f7ca97cd68b0a3a8694
chuyun028 <chuyun028@163.com>
2019-02-25 10:54:19 +0800

    修改文件内容--地址路径

--- emailTest/src/test/java/com/kgc/emailTest/service/MailServiceTest.java
+++ emailTest/src/test/java/com/kgc/emailTest/service/MailServiceTest.java
@@ -19,7 +19,7 @@ public class MailServiceTest {
      */
     @Test
     public void testSimpleMail() throws Exception {
-    	mailService.sendSimpleMail("1525031069@qq.com","test simple mail"," 测试发送简单邮件");
+    	mailService.sendSimpleMail("xx@qq.com","test simple mail"," 测试发送简单邮件");
     }
     /**
      * 2
@@ -32,7 +32,7 @@ public class MailServiceTest {
                 "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
                 "</body>\n" +
                 "</html>";
-        mailService.sendHtmlMail("1525031069@qq.com","test simple mail",content);
+        mailService.sendHtmlMail("xx@qq.com","test simple mail",content);
     }
     
     /**
@@ -41,6 +41,6 @@ public class MailServiceTest {
     @Test
     public void sendAttachmentsMail() {
         String filePath="e:\\tmp\\application.log";
-        mailService.sendAttachmentsMail("1525031069@qq.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
+        mailService.sendAttachmentsMail("xx@qq.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
     }
 }
